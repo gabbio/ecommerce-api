bbf32658b518c4cded91667d44539194518044d7
Abou Bakr Gbané <aboubakr.gbane@gmail.com>
2020-10-10 14:16:21 +0000

    chore: Set up project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+// Middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Routes
 app.get('/', (request, response) => {
