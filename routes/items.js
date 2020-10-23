d916d819cc0dd99676a65a57b0bf7bc18d1e60b5
Abou Bakr Gbané <aboubakr.gbane@gmail.com>
2020-10-23 15:16:38 +0000

    Protect all the relevant routes

--- routes/items.js
+++ routes/items.js
@@ -1,13 +1,19 @@
 const router = require('express').Router()
+const authRouter = require('express').Router()
+const auth = require('../middlewares/auth')
+const isAdmin = require('../middlewares/isAdmin')
+
+// Protect relevant routes
+authRouter.use(auth, isAdmin)
 
 // Item controller
 const itemsController = require('../controllers/itemsController')
 
 // Routes definition
 router.get('/', itemsController.getAllItems)
-router.post('/', itemsController.addItem)
+router.post('/', authRouter, itemsController.addItem)
 router.get('/:id', itemsController.getItem)
-router.put('/:id', itemsController.updateItem)
-router.delete('/:id', itemsController.deleteItem)
+router.put('/:id', authRouter, itemsController.updateItem)
+router.delete('/:id', authRouter, itemsController.deleteItem)
 
 module.exports = router
